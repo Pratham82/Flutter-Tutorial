f00d01ece2a3fdcfe3197d3666ee8ee99771ea15
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-05-14 23:53:13 +0530

    Added new routes and login page

--- 30-days-of-Futter/flutter_catalog/lib/main.dart
+++ 30-days-of-Futter/flutter_catalog/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/homePage.dart';
+import 'package:flutter_catalog/pages/loginPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
     );
   }
 }
