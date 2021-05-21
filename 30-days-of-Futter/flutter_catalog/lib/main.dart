e1c30a4ae64125a8c55f04160f6256b2bb69971d
prathameshedulab <prathamesh@edulab.in>
2021-05-21 23:34:17 +0530

    Added login page and routing

--- 30-days-of-Futter/flutter_catalog/lib/main.dart
+++ 30-days-of-Futter/flutter_catalog/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/homePage.dart';
 import 'package:flutter_catalog/pages/loginPage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+          primarySwatch: Colors.red,
+          fontFamily: GoogleFonts.poppins().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
         '/home': (context) => HomePage(),
