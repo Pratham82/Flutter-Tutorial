bc1ecd4dc8ac4a6e9709e1e64c014d5797fda275
prathameshedulab <prathamesh@edulab.in>
2021-05-21 23:33:59 +0530

    Added new changes to login page

--- 30-days-of-Futter/flutter_catalog/lib/pages/loginPage.dart
+++ 30-days-of-Futter/flutter_catalog/lib/pages/loginPage.dart
@@ -4,8 +4,49 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text('Login Page'),
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            'assets/images/login_img.png',
+            fit: BoxFit.cover,
+            height: 290,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 25),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: 'Enter Username', labelText: 'Username'),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: 'Enter Password', labelText: 'Password'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => print('onPressed called'),
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
